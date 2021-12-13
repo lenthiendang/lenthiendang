245a8b4b6e5d8e673e924dbb4855b477e9e8b8c0
caotrimai <caotri.mct@gmail.com>
2021-12-13 17:31:35 +0700

    Update register room when not matching redux room and pattern room

--- src/renderer/domains/Awakening/MainLayout.jsx
+++ src/renderer/domains/Awakening/MainLayout.jsx
@@ -7,16 +7,29 @@ import {
   updatePatternList,
 } from '../../redux/slices/awakeningSlice';
 import { SocketContext } from '../../socket';
+import { PATTERN_TYPE } from './awakeningUtil';
 import RandomAwakening from './components/RandomAwakening';
-import TestAwaken from './testing';
 import useAwakenSocket from './hooks/useAwakenSocket';
 
 function MainLayout() {
   const dispatch = useDispatch();
   const candles = useSelector((state) => state.price.list);
-  const patternList = useSelector((state) => state.awakening.patternList);
+  const {
+    patternList,
+    roomList,
+    commonParoliRunning,
+    commonParoliFunds,
+  } = useSelector((state) => state.awakening);
   const isRunning = patternList.some((pattern) => pattern.isActive === true);
   const socket = useContext(SocketContext);
+
+  const reduxRoomList = roomList.map(({ roomId }) => roomId) || []
+  const patternRooms = patternList.filter(
+    (pattern) => pattern.type === PATTERN_TYPE.COMMON_PAROLI
+  ).map(({ roomId }) => roomId) || [];
+  const roomMatching = patternRooms.every(
+    (roomId) => reduxRoomList.includes(roomId)
+  )
 
   useAwakenSocket();
 
@@ -42,6 +55,23 @@ function MainLayout() {
       clearTimeout(timeoutId);
     }, 1500);
   }, [dispatch, candles]);
+
+  // re-register when the rooms do not match
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (
+        commonParoliRunning
+        && patternRooms.length > 0
+        && reduxRoomList.length > 0
+        && !roomMatching
+      ) {
+        socket.emit('AWAKEN_UNREGISTER');
+        const patternAmount = Math.floor(commonParoliFunds);
+        socket.emit('AWAKEN_REGISTER', patternAmount);
+      }
+      clearTimeout(timeoutId);
+    }, 3000);
+  }, [candles]);
 
   return (
     <>
